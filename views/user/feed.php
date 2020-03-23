9849ba60031e3d39b05249ae44ee511aff886782
Marcus Bjerringgaard <mb@tutee.dk>
2020-03-23 22:40:34 +0100

    Setup Project and branch

---
+++ views/user/feed.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/feed.css">
+
+	<title>Feed</title>
+</head>
+<body>
+<div id="wrapper"> 
+    
+<header> 
+<div id="logo">ShortCircuit</div>
+<nav>
+  <a href="#" class="menu-trigger"><i class="fa fa-bars fa-2x" aria-hidden="true"></i></a>
+  <ul>
+	<li><a href="index.html"><i class="fas fa-home"></i></a></li>
+	<li><a href="#categories"><i class="fas fa-compass"></i></a></li>
+	<li><a href="#pinned"><i class="fas fa-thumbtack"></i></a></li>
+      <li><a href="#profile"><i class="fas fa-user-circle"></i></a></li>
+      <li><a href="#settings"><i class="fas fa-cog"></i></a></li>
+  </ul>
+</nav>
+</header> 
+
+</body>
+</html>
+
+
+<?php
