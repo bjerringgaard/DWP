a6747c707f14c6927d9df27e4fecb81157d161e5
Peter <thewoet@gmail.com>
2020-04-15 12:44:55 +0200

    userPage & userPost

---
+++ views/admin/userPage.php
@@ -0,0 +1,73 @@
+<?php
+require("../../Includes/connection.php");
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Page</title>
+    <meta name="description" content="Admin Dashboard">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e0fef6fe5f.js" crossorigin="anonymous"></script>
+  </head>
+
+<body>
+<div id="wrapper">   
+<header> 
+<?php include 'includes/header.php';?>
+</header> 
+
+<section id="main">
+<div class="box1">
+  <h2>Users</h2>
+
+
+
+
+  <div class="manageMember">
+    <a href="create.php"><button type="button">Add Member</button></a>
+    <table border="1" cellspacing="0" cellpadding="0">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Contact</th>
+                <th>Option</th>
+            </tr>
+        </thead>
+        <tbody>
+             
+        </tbody>
+    </table>
+</div>
+
+</div>
+
+
+
+
+<hr class="new1">
+</div>
+
+
+
+
+
+
+</section>
+
+  
+
+<section id="sb">
+<?php include 'includes/navigation.php';?>
+</section>
+<footer>
+     <div class="footer_information">
+        <p>© Peter Schaadt Wind</p>
+    </div> 
+</footer>
+</div>   
+</body>
+</html>    
