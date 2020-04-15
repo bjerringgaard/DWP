affec73fe60dd7a2e56de64af2ab6df8b43365d7
Marcus Bjerringgaard <mb@tutee.dk>
2020-04-15 11:16:39 +0200

    Changed DB Connection
    Changed DB Connection to matcher DWP user

--- Includes/constants.php
+++ Includes/constants.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_SERVER","localhost");
-define("DB_USER", "root");
-define("DB_PASS", "root");
+define("DB_USER", "DWP");
+define("DB_PASS", "123456");
 define("DB_NAME", "shortcircuit");
