8bf9295edde2f2be1d9666e85709ca8dada339d2
Peter <thewoet@gmail.com>
2020-04-23 13:40:56 +0200

    OOP DB connect & Show Comments from Classes

---
+++ views/admin/includes/commentsPage.php
@@ -0,0 +1,16 @@
+<?php
+
+class CommentsPage extends DbClass  {
+
+    protected function getAllComments(){
+        $sql = "SELECT * from comment";
+        $result = $this->connect()->query($sql);
+        $numRows = $result->num_rows;
+        if($numRows > 0){
+            while($row = $result->fetch_assoc()){
+             $data[] = $row;
+            }
+            return $data;
+        }
+    }
+}
