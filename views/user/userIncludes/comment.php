5703d45e770b6e2bbb42b320c202a3c197d23951
Marcus Bjerringgaard <mb@tutee.dk>
2020-05-14 11:06:19 +0200

    Feed update

--- views/user/userIncludes/comment.php
+++ views/user/userIncludes/comment.php
@@ -1,29 +1,26 @@
 <?php
 if(isset($_POST["submit"])){
-	$localfile = $_FILES["file_1"];
-	var_dump($localfile);
-
-    if((($_FILES["file"]["type"] == "image/jpeg")
-        or($_FILES["file"]["type"] == "image/gif")
-        or($_FILES["file"]["type"] == "image/png")
-        or($_FILES["file"]["type"] == "image/jpg"))
-        && ($_FILES["file"]["size"]<2500000000)){
+    if((($_FILES[1]["file"]["type"] == "image/jpeg")
+        or($_FILES[1]["file"]["type"] == "image/gif")
+        or($_FILES[1]["file"]["type"] == "image/png")
+        or($_FILES[1]["file"]["type"] == "image/jpg"))
+        && ($_FILES[1]["file"]["size"]<2500000000)){
 					
 
-if($_FILES["file"]["error"]>0){
-    echo "Error: ". $_FILES["file"]["error"]."<br>";
+if($_FILES[1]["file"]["error"]>0){
+    echo "Error: ". $_FILES[1]["file"]["error"]."<br>";
 }else{
-	echo "Upload: ". $_FILES["file"]["name"]."<br>";
-	echo "Type: ". $_FILES["file"]["type"]."<br>";
-	echo "Size: ". ($_FILES["file"]["size"]/1024)."<br>";
-	echo "Temp folder: ". $_FILES["file"]["tmp_name"]."<br>";
+	echo "Upload: ". $_FILES[1]["file"]["name"]."<br>";
+	echo "Type: ". $_FILES[1]["file"]["type"]."<br>";
+	echo "Size: ". ($_FILES[1]["file"]["size"]/1024)."<br>";
+	echo "Temp folder: ". $_FILES[1]["file"]["tmp_name"]."<br>";
 
-			if (file_exists("../../uploads/comments/".$_FILES["file"]["name"])){
+			if (file_exists("../../uploads/comments/".$_FILES[1]["file"]["name"])){
 					echo "Findes allerede";
 			}else{
-					move_uploaded_file($_FILES["file"]["tmp_name"],
-					"../../uploads/comments/".$_FILES["file"]["name"]);
-		echo "stored in Commentupload: ". $_FILES["file"]["name"];
+					move_uploaded_file($_FILES[1]["file"]["tmp_name"],
+					"../../uploads/comments/".$_FILES[1]["file"]["name"]);
+		echo "stored in Commentupload: ". $_FILES[1]["file"]["name"];
 		
 		$sql = "INSERT INTO Comment (UserID, CommentText, CmtAttachement, CommentStyle, CommentTimeStamp, PostID) 
 						VALUES ('Bbaggz', '".$_POST["commentText"]."', '".$_FILES["file"]["name"]."', NULL, CURRENT_TIMESTAMP, ".$_POST["PostID"].")"; 
@@ -37,6 +34,5 @@ if($_FILES["file"]["error"]>0){
 					VALUES ('Bbaggz', '".$_POST["commentText"]."', 'test1', NULL, CURRENT_TIMESTAMP, ".$_POST["PostID"].")"; 
 						mysqli_query($conn, $sql);
 						mysqli_close($conn);	
-							//header("Location: feed.php");
-							echo "Error: ". $_FILES["file"]["error"]."<br>";
+							header("Location: feed.php");
     }}
