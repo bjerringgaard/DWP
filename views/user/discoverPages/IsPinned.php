c66af74e6cf09f6b5d4cdcaf35a2bf7ef386c8a7
Marcus Bjerringgaard <mb@tutee.dk>
2020-05-14 14:34:44 +0200

    Finished Discover Paages

--- views/user/discoverPages/IsPinned.php
+++ views/user/discoverPages/IsPinned.php
@@ -33,79 +33,88 @@ include("../userIncludes/comment.php")
 		// output data of each row
 		while($post = mysqli_fetch_assoc($result)) {
 			echo '
-				<div id="post">
-					<div id="postFrame">
-						<div class="postImg">
-							<img id="theImage" src="../../../uploads/posts/' . $post["PostImage"] . '" alt="">
-						</div>
+<div id="post">
+	<div id="postFrame">
+		<div class="postImg"><img id="theImage" src="../../../uploads/posts/' . $post["PostImage"] . '" alt=""></div>
+		<div class="postTitle"><h3>' . $post["PostTitle"] . '</h3></div>
+		<div class="postInfo">
+			<div class="postStats">
+				<p>' . $post["PostLikes"] . ' LIKES</p>
+				<p>&bull;</p>
+				<p id="postTime">' . timeAgo($post["PostTime"]) . '</p>
+			</div>
+			<div class="postAction">
+					<a href="#" id="like"><i class="fas fa-arrow-alt-circle-up"></i></a>
+					<a href="#"id="comment"><i class="fas fa-arrow-alt-circle-down"></i></a>
+					<a href="#" id="Pin"><i class="fas fa-thumbtack"></i></a>
+			</div>
+		</div>
 
-						<div class="postTitle"><h3>' . $post["PostTitle"] . '</h3></div>
-						<div class="postInfo">
-							<div class="postStats">
-								<p>' . $post["PostLikes"] . ' LIKES</p>
-								<p>&bull;</p>
-								<p id="postTime">' . timeAgo($post["PostTime"]) . '</p>
-							</div>
-							<div class="postAction">
-									<a href="#" id="like"><i class="fas fa-arrow-alt-circle-up"></i></a>
-									<a href="#"id="comment"><i class="fas fa-arrow-alt-circle-down"></i></a>
-									<a href="#" id="Pin"><i class="fas fa-thumbtack"></i></a>
-							</div>
-						</div>
-
-						<div class="postUser">
-						<div class="profilePic"><img src="' . $post["ProfilePic"] . '" alt=""></div>
-							<div>
-								<h3>' . $post["UserID"]. '</h3>
-								<p>' . $post["PostDesc"] . '</p>
-							</div>
-						</div>
-					</div>
+		<div class="postUser">
+			<div class="profilePic"><img src="' . $post["ProfilePic"] . '" alt=""></div>
+			<div>
+				<h3>' . $post["UserID"]. '</h3>
+				<p>' . $post["PostDesc"] . '</p>
+			</div>
+		</div>
+	</div>
 
 
-					<div id="postCommentFrame">
-							<div id="commentField">
-								<h3>Comments</h3>
-							
-								<div class="commentUser"> ';
+	<div id="postCommentFrame">';
+		$asql = "SELECT COUNT(*) As CommentAmount
+				FROM Comment
+				WHERE PostID=" . $post["PostID"];
+				$aresult = mysqli_query($conn, $asql);
+				
+				if (mysqli_num_rows($aresult) > 0) {
+					$cmtAmount = mysqli_fetch_assoc($aresult);
+					echo '<h3>' . $cmtAmount["CommentAmount"] . ' Comments</h3>';
+					
+				}else{
+					echo '<h3> 0 Comments</h3>';
+				}
+		echo '
+		<div id="commentField">';
+			$csql = "SELECT * 
+			FROM Post p, Comment c, User u
+			WHERE c.PostID = p.PostID
+			AND c.UserID = u.UserID
+			AND p.PostID = " . $post["PostID"];
 
-								$csql = "SELECT * 
-								FROM Post p, Comment c, User u
-								WHERE c.PostID = p.PostID
-								AND c.UserID = u.UserID
-								AND p.PostID = " . $post["PostID"];
+			$cresult = mysqli_query($conn, $csql);
 
-								$cresult = mysqli_query($conn, $csql);
+				if (mysqli_num_rows($cresult) > 0) {
+					while($comment = mysqli_fetch_assoc($cresult)) {
+						echo '
+			<div class="commentUser">
+				<div class="theComment">
+					<div class="profilePic"><img src="' . $comment["ProfilePic"] . '" alt=""></div>
+					<div>
+						<h3>' . $comment["UserID"]. '</h3>
+						<p>' . $comment["CommentText"] . '</p>
+						<div class="commentImg"><img id="theCmtImage" src="../../../uploads/comments/' . $comment["CmtAttachement"] . '" alt=""></div>
+				 </div>
+			</div> 
+		</div>
+		'; 
+					} 
+				}
 
-								if (mysqli_num_rows($cresult) > 0) {
-									while($comment = mysqli_fetch_assoc($cresult)) {
-										echo '
-											<div class="theComment">
-												<div class="profilePic"><img src="' . $comment["ProfilePic"] . '" alt=""></div>
-													<div>
-														<h3>' . $comment["UserID"]. '</h3>
-														<p>' . $comment["CommentText"] . '</p>
-													</div>
-											</div> '; 
-									} 
-								}
-
-
-							echo '</div>	
-							</div>
-							<div id="writeComment">
-							<form action="" method="post" enctype="multipart/form-data">
-								<input type="hidden" id="PostID" name="PostID" value="' . $post["PostID"] . '">
-								<input type="text" name="commentText">
-								<input type="file" name="file" id="file"></input>
-								<label for="file"><i class="fas fa-paperclip"></i></label>
-								<input type="submit" name="submit" class="btn fa-input" value="send"></input>
-							</form>
-							</div>
-						</div>
-				</div>	';
-		}
-	} else {
-		echo "0 results";
-	} ?>
+		echo ' </div>
+		<div id="writeComment">
+			<form action="" method="POST" enctype="multipart/form-data" name="' . $post["PostID"] . '">
+				<input type="hidden" id="PostID" name="PostID" value="' . $post["PostID"] . '">
+				<input type="text" name="commentText">
+				<input type="file" name="file[]" id="file"></input>
+				<label for="file"><i class="fas fa-paperclip"></i></label>
+				<input type="submit" name="submit" class="btn fa-input" value="send"></input>
+			</form>
+			</div>
+		</div>
+	</div>
+</div>';
+	}
+} else {
+	echo "0 results";
+} ?>
 	</section>
